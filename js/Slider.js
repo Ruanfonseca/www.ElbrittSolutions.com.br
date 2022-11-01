acfa67f06fc9e5026ff235e7b0bdee027fab389c
Ruan fonseca <whitelook22@outlook.com>
2022-11-01 12:57:29 -0300

    correções de conteudo

--- js/Slider.js
+++ js/Slider.js
@@ -1,7 +1,7 @@
 (function(){
     
-    const sliders = [...document.querySelectorAll('.selecaoServicosBody')];
-    const buttonNext = document.querySelector('#next');//pegando o clique do botão
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
     const buttonBefore = document.querySelector('#before');
     let value;   
 
@@ -14,17 +14,17 @@
     });
 
     const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('.selecaoServicosBody-show').dataset.id;
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
         value = Number(currentTestimony);
         value+= add;
 
 
-        sliders[Number(currentTestimony)-1].classList.remove('selecaoServicosBody-show');
-        if(value === sliders.length+1 || value === 0){//se o valor for igual ao numero de elementos no array de slides
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+        if(value === sliders.length+1 || value === 0){
             value = value === 0 ? sliders.length  : 1;
         }
 
-        sliders[value-1].classList.add('selecaoServicosBody-show');
+        sliders[value-1].classList.add('testimony__body--show');
 
     }
 
